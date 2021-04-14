82b7b1110f5b66b27b10cae6dded5ef52bfc0061
notLuckyA <aleks.1606sh@gmail.com>
2021-04-14 14:58:59 +0300

    normal input nickname

--- client/HTTPClient.java
+++ client/HTTPClient.java
@@ -3,9 +3,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class HTTPClient {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         Socket sock = new Socket("192.168.43.91",
                 8080);
 
@@ -13,16 +14,22 @@ public class HTTPClient {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        System.out.print("NAME: ");
+        Scanner scanner = new Scanner(System.in);
+        String nick = scanner.nextLine();
+
+
         String message;
-        String nick = null;
-        while ((message = br.readLine()) != null){
-            if (message.length() == 8){
+        message = nick + "\n";
+        //System.out.print(nick + "\n");
+        output.write(message.getBytes());
+
+        while ((message = br.readLine()) != null) {
+            if (message.length() == 8) {
                 output.write("\n".getBytes());
                 break;
             }
-            if (nick == null){
-                nick = message;
-            }
+
             message = message + "\n";
             output.write(message.getBytes());
         }
