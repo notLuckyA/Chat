b671a01a1ce1939e5d97fbd75d4f805e54978495
notLuckyA <aleks.1606sh@gmail.com>
2021-04-15 12:43:44 +0300

    multithreading implemented

--- server/Server.java
+++ server/Server.java
@@ -1,29 +1,47 @@
 import java.io.*;
 import java.net.*;
+
 public class Server {
+
+    public static class ClientThread extends Thread {
+        private Socket clientSocket;
+
+        public ClientThread(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        public void run() {
+            try {
+                OutputStream output = clientSocket.getOutputStream();
+                InputStream input = clientSocket.getInputStream();
+                BufferedReader br = new BufferedReader((new InputStreamReader(input)));
+
+                String nick = br.readLine();
+                System.out.println("--> " + nick + " вошёл в чат");
+                // проверка Git в IDE
+                String message;
+                while ((message = br.readLine()) != null) {
+                    if (message.equals("\n")) {
+                        break;
+                    }
+                    System.out.println(nick + ": " + message);
+                }
+                System.out.print("<-- " + nick + " вышел из чата");
+                output.close();
+                input.close();
+            } catch (IOException e) {
+                System.out.println("Error" + e);
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8080);
-        while (true){
+        while (true) {
             Socket clientSocket = serverSocket.accept();
 
-            OutputStream output = clientSocket.getOutputStream();
-            InputStream input = clientSocket.getInputStream();
-            BufferedReader br = new BufferedReader((new InputStreamReader(input)));
-
-            String nick = br.readLine();
-            System.out.println("--> " + nick + " вошёл в чат");
-
-
-            String message;
-            while ((message = br.readLine()) != null){
-                if (message.equals("\n")){
-                    break;
-                }
-                System.out.println(nick + ": " + message);
-            }
-            System.out.print("<-- " + nick + " вышел из чата");
-            output.close();
-            input.close();
+            ClientThread t = new ClientThread(clientSocket);
+            t.start();
         }
     }
 }
